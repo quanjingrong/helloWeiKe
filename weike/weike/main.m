dc5eefa3d9275923a8a5deb8c31afbe4afd48752
quanjingrong <793572071@qq.com>
2016-06-13 19:18:33 +0800

    hello

---
+++ weike/weike/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weike
+//
+//  Created by 全景荣 on 16/6/13.
+//  Copyright © 2016年 NSOperation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
